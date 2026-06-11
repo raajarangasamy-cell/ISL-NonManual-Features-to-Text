c50798d483a251cafa419aa930bfcb9605c51cfc
Raja R <raajarangasamy@gmail.com>
2026-06-11 21:43:00 +0530

    feat: connect text conversion to head movement detection

--- src/utils/head_detector.py
+++ src/utils/head_detector.py
@@ -1,4 +1,8 @@
-# Head Movement Detection Module
+from utils.text_converter import convert_to_text
+
+def detect_head_movement():
+    print("Detecting head movements...")
+    convert_to_text()# Head Movement Detection Module
 
 def detect_head_movement():
     print("Detecting head movements...")
