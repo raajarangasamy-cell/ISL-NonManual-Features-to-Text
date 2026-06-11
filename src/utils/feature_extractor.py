4ec37005129cb959d8605d4843fbe0d76be12e40
Raja R <raajarangasamy@gmail.com>
2026-06-11 21:38:21 +0530

    feat: connect face detection to feature extraction

--- src/utils/feature_extractor.py
+++ src/utils/feature_extractor.py
@@ -1,4 +1,8 @@
-# Feature Extraction Module
+from utils.face_detector import detect_face
+
+def extract_features():
+    print("Extracting non-manual features...")
+    detect_face()# Feature Extraction Module
 
 def extract_features():
     print("Extracting non-manual features...")
