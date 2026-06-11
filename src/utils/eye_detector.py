fccb416208f5cc8d1b6351fd145b7ea448e42066
Raja R <raajarangasamy@gmail.com>
2026-06-11 21:41:18 +0530

    feat: connect head movement detection to eye detection

--- src/utils/eye_detector.py
+++ src/utils/eye_detector.py
@@ -1,4 +1,8 @@
-# Eye Movement Detection Module
+from utils.head_detector import detect_head_movement
+
+def detect_eyes():
+    print("Detecting eye movements...")
+    detect_head_movement()# Eye Movement Detection Module
 
 def detect_eyes():
     print("Detecting eye movements...")
